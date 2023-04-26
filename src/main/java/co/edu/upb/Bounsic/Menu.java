baf3226fbf2a09b4a4ba3db1046d5b917385ac18
jubelt <127954893+jubelt@users.noreply.github.com>
2023-04-25 23:09:16 -0500

    Update Menu.java

--- src/main/java/co/edu/upb/Bounsic/Menu.java
+++ src/main/java/co/edu/upb/Bounsic/Menu.java
@@ -13,31 +13,37 @@ public class Menu {
             System.out.println("1. Buscar canción");
             System.out.println("2. Ver la biblioteca de canciones");
             System.out.println("3. Cancion aleatoria");
-            System.out.println("4. Crear lista de 7 canciones aleatorias");
-            System.out.println("5. Cerrar sesión");
+            System.out.println("4. Lista de hits");
+            System.out.println("5. Crear lista de 7 canciones aleatorias");
+            System.out.println("6. Cerrar sesión");
             opcion = sc.nextLine();
 
             switch (opcion) {
                 case "1":
                     MetodosMenu.buscarCacion(biblioteca, sc);
                     break;
-                //-------------------------------------------------------------------------------------//	
+                //-------------------------------------------------------------------------------------//
                 case "2":
                     MetodosMenu.verBibliotecaCanciones(biblioteca, sc);
                     break;
                 //-------------------------------------------------------------------------------------//
                 case "3":
-                    MetodosMenu.cancionAleatoria(biblioteca, sc);
+                    MetodosMenu.cancionAleatoria(biblioteca);
                     break;
                 //-------------------------------------------------------------------------------------//
                 case "4":
-                    MetodosMenu.crearListaCancionesAleatorias(biblioteca, sc);                    
+                    MetodosMenu.listaReproduccionHits(biblioteca, sc);
                     break;
                 //-------------------------------------------------------------------------------------//
                 case "5":
-                    System.out.println("Sesión cerrada.");
-                    menu = false;                    
+                    MetodosMenu.crearListaCancionesAleatorias(biblioteca, sc);
                     break;
+                //-------------------------------------------------------------------------------------//
+                case "6":
+                    System.out.println("Sesión cerrada.");
+                    menu = false;
+                    break;
+                //-------------------------------------------------------------------------------------//
                 default:
                     System.out.println("Opción no válida. Intente de nuevo.");
                     break;
